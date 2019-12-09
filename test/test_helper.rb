103a542c9c412ee614d218f9d049b8b29af92df3
Petr Ablyazov <phoenix-h@mail.ru>
2019-12-09 10:50:41 +0400

    Add simplecov in test_helper. Add branch in .travis.yml

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,7 @@
+require 'simplecov'
 require 'coveralls'
 ENV['RAILS_ENV'] ||= 'test'
+SimpleCov.start 
 Coveralls.wear!('rails')
 require_relative '../config/environment'
 require 'rails/test_help'
