489dc833bb953b97b7b5a007e7d061010d193b5b
Petr Ablyazov <phoenix-h@mail.ru>
2019-12-14 15:08:52 +0400

    Create and Update

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -6,15 +6,37 @@ class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
     sign_in_as admin
   end
 
+  test "should get index" do
+    get admin_users_url
+    assert_response :success
+  end
+
   test "should get show" do
     user = create(:user)
     get admin_user_url(user)
     assert_response :success
   end
 
-  test "should get index" do
-    get admin_users_url
+  test "should get edit" do
+    user = create(:user)
+    get edit_admin_user_url user.id
     assert_response :success
   end
-  
+
+  test "should get new" do
+    get new_admin_user_url
+    assert_response :success
+  end
+
+  test "should post create" do
+    post admin_users_url, params: { user: attributes_for(:user) }
+    assert_response :redirect
+  end
+
+  test "should patch update" do
+    user = create(:user)
+    user_attrs = attributes_for(:user)
+    patch admin_user_url user.id, params: { user: user_attrs }
+    assert_response :redirect
+  end
 end
