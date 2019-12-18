7e49b72beb74910478756a520ac5742908a60ebf
Petr Ablyazov <phoenix-h@mail.ru>
2019-12-18 10:50:04 +0400

    Add index, show methods in controller. Create serializer.

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,2 +1,25 @@
-class V1::TasksController < V1::ApplicationController
+class Api::V1::TasksController < Api::V1::ApplicationController
+  respond_to :html, :json
+
+  def index
+    q_params = params[:q] || { s: 'id asc' }
+
+    tasks = Task.all
+      .ransack(q_params)
+      .result
+      .page(params[:page])
+      .per(params[:per_page])
+
+    json = {
+        items: tasks.map { |t| TaskSerializer.new(t).as_json },
+        meta: build_meta_tasks(tasks)
+    }
+
+    respond_with json
+  end
+
+  def show
+    task = Task.find(params[:id])
+    respond_with(task)
+  end
 end
